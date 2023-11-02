16cb904896c7f7c0ab25f0b7478f84a3b4973544
AMALU SAJEEV <amalu.sajeev.me@gmail.com>
2023-11-02 23:32:35 +0530

    minor fixes

--- src/components/FileIcon.tsx
+++ src/components/FileIcon.tsx
@@ -1,7 +1,6 @@
-import { Article, CheckBox, PageviewOutlined, PictureAsPdfTwoTone } from "@mui/icons-material";
-import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControlLabel, Tooltip, Typography } from "@mui/material";
-import React, { useState } from "react";
-import { PageSelectionBox } from "./PageSelectionBox";
+import { PictureAsPdfTwoTone } from "@mui/icons-material";
+import { Box, Tooltip, Typography } from "@mui/material";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 
 interface IFileIconProps {
--- src/components/dashboard/ListItems.tsx
+++ src/components/dashboard/ListItems.tsx
@@ -2,10 +2,6 @@ import * as React from 'react';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import ListSubheader from '@mui/material/ListSubheader';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import LayersIcon from '@mui/icons-material/Layers';
-import AssignmentIcon from '@mui/icons-material/Assignment';
 import { Folder, LogoutTwoTone, UploadFile } from '@mui/icons-material';
 
 export const mainListItems = (
